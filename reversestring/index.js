1781ad58b693b25eaa07a905d18433d0308e21df
Geoffrey <geoffreyplichard@gmail.com>
2018-12-31 17:43:58 +0100

    Reverse string solution 2

--- reversestring/index.js
+++ reversestring/index.js
@@ -6,8 +6,33 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
+
+
+// Solution 1. Turn the string into an array, then reverse it and join to a string again.
+
 function reverse(str) {
     return str.split('').reverse().join('');
 }
+
+
+
+
+// Solution 2. Without the reverse helper. Create an empty string, concat each letter with the temp string
+// Ex: apple
+// a
+// pa
+// ppa
+// lppa
+// elppa
+
+// function reverse(str) {
+//     let reversed = '';
+
+//     for (let character of str) {
+//         reversed = character + reversed;
+//     }
+
+//     return reversed;
+// }
 
 module.exports = reverse;
