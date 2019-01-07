e5a0813bbdd8f796b1cef50bdf2e7e755d11646c
Geoffrey <geoffreyplichard@gmail.com>
2019-01-07 19:51:16 +0100

    Anagrams solution 2

--- anagrams/index.js
+++ anagrams/index.js
@@ -10,32 +10,47 @@
 
 
 // SOLUTION 1 - Using char maps
-function anagrams(stringA, stringB) {
-    const aCharMap = buildCharMap(stringA);
-    const bCharMap = buildCharMap(stringB);
 
-    if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
-        return false;
-    }
+// function anagrams(stringA, stringB) {
+//     const aCharMap = buildCharMap(stringA);
+//     const bCharMap = buildCharMap(stringB);
 
-    for (let char in aCharMap) {
-        if (aCharMap[char] !== bCharMap[char]) {
-            return false;
-        }
-    }
+//     if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
+//         return false;
+//     }
 
-    return true;
-}
+//     for (let char in aCharMap) {
+//         if (aCharMap[char] !== bCharMap[char]) {
+//             return false;
+//         }
+//     }
+
+//     return true;
+// }
 
 // Helper function
-function buildCharMap(str) {
-    const charMap = {};
+// function buildCharMap(str) {
+//     const charMap = {};
     
-    for (let char of str.replace(/[^\w]/g, '').toLowerCase()) {
-        charMap[char] = charMap[char] + 1 || 1;
-    }
+//     for (let char of str.replace(/[^\w]/g, '').toLowerCase()) {
+//         charMap[char] = charMap[char] + 1 || 1;
+//     }
 
-    return charMap;
+//     return charMap;
+// }
+
+
+
+
+// SOLUTION 2 - Using sort helper
+
+function anagrams(stringA, stringB) {
+    return cleanAndSortString(stringA) === cleanAndSortString(stringB);
+
+}
+
+function cleanAndSortString(str) {
+    return str.replace(/[^\w]/g, '').toLowerCase().split('').sort().join('');
 }
 
 module.exports = anagrams;
