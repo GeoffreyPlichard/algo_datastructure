e41b34dbf0372e32b5ab07ce79302a25a501ffd1
Geoffrey <geoffreyplichard@gmail.com>
2019-01-28 21:18:40 +0100

    Steps recursion solution

--- steps/index.js
+++ steps/index.js
@@ -21,14 +21,34 @@
 // Solution 1 - Handle it like a 2 dimension array and itérate on row and cols
 // if the current col is <= to the current row, add a #, else add a space
 
-function steps(n) {
-    for (let row = 0; row < n; row++) {
-        let str = "";
-        for (let column = 0; column < n; column++) {
-            column <= row ? str += "#" : str += " ";
-        }
-        console.log(str);
+// function steps(n) {
+//     for (let row = 0; row < n; row++) {
+//         let str = "";
+//         for (let column = 0; column < n; column++) {
+//             column <= row ? str += "#" : str += " ";
+//         }
+//         console.log(str);
+//     }
+// }
+
+// Solution 2 - Using recursion
+
+function steps(n, row = 0, stair = '') {
+    if (n === row) {
+        return;
     }
+
+    if (n === stair.length) {
+        console.log(stair);
+        return steps(n, row + 1);
+    }
+
+    if (stair.length <= row) {
+        stair += '#';
+    } else {
+        stair += ' ';
+    }
+    steps(n, row, stair);
 }
 
 
